b72a9df1996f1ba9cb4d1bafdd1bd08d41265d20
The Beard of Knowledge <thebeardofknowledge956@gmail.com>
2025-09-05 18:28:27 -0500

    Delete Fix-Windows-Corruption/WMI corruption directory

--- Fix-Windows-Corruption/WMI corruption/fixWMIcorruption.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-::WMIcorruptionfix
-echo Checking / Repairing Windows Management Instrumentation
-	cd C:\Windows\System32\Wbem
-	for /f %%s in ('dir /b *.mof *.mfl') do mofcomp %%s
-	for %%i in (*.dll) do regSvr32 -s %%i)
-	net stop winmgmt /y
-	net start winmgmt
